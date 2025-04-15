25c9a829b31a4443712f0373af92bf227f0797bc
Juan Fonseca <juanof2000@gmail.com>
2025-04-15 20:04:03 -0300

    fix dockerfile, create docker ignore

--- obj/Debug/net8.0/Library.API.AssemblyInfo.cs
+++ obj/Debug/net8.0/Library.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Library.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91f6490b06eaf1e4c3f28dc9a6760efca410d874")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03b5c5975ab32f53a95948e8bcfd613243c40417")]
 [assembly: System.Reflection.AssemblyProductAttribute("Library.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Library.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
